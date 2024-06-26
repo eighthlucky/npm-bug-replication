449424f56ade6129bf34a6725b95576e71d0b379
Peter Tran <24271588+eighthlucky@users.noreply.github.com>
2024-06-27 00:45:46 +1000

    feat: replicate uWebSockets error message
    This replicates the following message:
    
    ```
    Error: uWS.HttpResponse must not be accessed after uWS.HttpResponse.onAborted callback,
    or after a successful response. See documentation for uWS.HttpResponse and consult
    the user manual.
    ```

---
+++ main.js
@@ -0,0 +1,29 @@
+// @ts-check
+
+import { Server } from 'socket.io';
+import { connect } from 'socket.io-client';
+import { App as createApp } from 'uWebSockets.js';
+
+const PORT = 3000;
+const SERVER_URL = `http://localhost:${PORT}`;
+
+// Initialize socket.io server with uWebSockets app
+const server = new Server({ serveClient: false });
+const app = createApp();
+server.attachApp(app);
+
+// Listen and wait for it to be ready
+await new Promise((r) => app.listen(PORT, r));
+
+// Create a new socket.io client and wait for it to connect
+const socket = connect(SERVER_URL);
+await new Promise((r) => socket.once('connect', r));
+
+// Mimic a new socket.io client connection with an already connected session ID
+// Engine.io will close the connection, but will incorrectly attempt to upgrade it afterwards
+// See https://github.com/socketio/engine.io/blob/6.6.0/lib/userver.ts#L213
+try {
+  new WebSocket(
+    `ws://localhost:3000/socket.io/?EIO=4&transport=websocket&sid=${socket.io.engine.id}`,
+  );
+} catch {}
